a2b4e9f2a7d677668dab993c4047490d57427f9b
Paul Massardo <paul.massardo@gmail.com>
2017-10-16 23:50:59 -0400

    Added Processing and Output
    Added Processing and Output

--- TaxCalculator/Program.cs
+++ TaxCalculator/Program.cs
@@ -90,7 +90,7 @@ namespace TaxCalculator
             const double levelThirdIncome = 25000.00;   // Constant that holds the third income threshold
             const double levelFourthIncome = 30000.00;  // Constant that holds the fourth income threshold
             const double levelFifthIncome = 40000.00;   // Constant that holds the fifth income threshold
-            const double levelSixthIncome = 50000.00;   // Constant that holds the sixth income threshold
+            //const double levelSixthIncome = 50000.00;   // Constant that holds the sixth income threshold
 
             const double incomeMinimum = 1.00;          // Constant that holds the income minimum
             const double incomeMaximum = 1000000.00;    // Constant that holds the income maximum
@@ -109,16 +109,17 @@ namespace TaxCalculator
             const double rateFifthTax = 0.3;            // Constant that holds the fifth tax rate
             const double rateSixthTax = 0.35;           // Constant that holds the sixth tax rate
 
-            const string messageOwe = "owe";            // Constant that holds the owe message
-            const string messageAreOwed = "are owed";   // Constant that holds the are owed message
+            const string messageOwe = "owe";                // Constant that holds the owe message
+            const string messageAreOwed = "are owed";       // Constant that holds the are owed message
+            const string messageOweNothing = "owe nothing"; // Constant that holds the owe nothing message
 
             ///////////////
             // Variables
             ///////////////
 
             double totalYearlyIncome = 0.00;            // Variable to hold the total yearly income, input by user
-            double totalTaxDeductions = 0.00;           // Variable to hold the total tax dedcutions, input by user
             double taxesPaidToDate = 0.00;              // Variable to hold the total taxes paid to date, input by user
+            double totalTaxDeductions = 0.00;           // Variable to hold the total tax dedcutions, input by user
             double totalTaxableIncome = 0.00;           // Variable to hold the total taxable income, calculated in process
             double totalTaxPayable = 0.00;              // Variable to hold the total tax payable, calculated in process
             double applicableTaxRate = 0.00;            // Variable to hold the applicable tax rate, determined in process
@@ -130,12 +131,10 @@ namespace TaxCalculator
             // Input
             ///////////////
 
-
             // Prompt and store the person's total yearly income in the totalYearlyIncome variable 
             // after the user input is converted to a double and 
             // it is validated agianst the income minimum and maximum range
             // and we also know it is a number.
-
             Console.Write("Please enter the person's total yearly income \n\t(between {0:c} and {1:c}): ", incomeMinimum, incomeMaximum ); // prompt the total yearly income
 
             // Keep asking the user until they input the correct total yearly income
@@ -148,12 +147,10 @@ namespace TaxCalculator
 
             }
 
-
             // Prompt and store the person's total taxes paid to date in the taxesPaidToDate variable 
             // after the user input is converted to a double and 
             // it is validated agianst the taxes paid to date minimum and maximum range
             // and we also know it is a number.
-
             Console.Write("Please enter the person's total taxes paid to date \n\t(between {0:c} and {1:c}): ", taxesPaidToDateMinimum, taxesPaidToDateMaximum); // prompt for the total taxes paid to date
 
             // Keep asking the user until they input the correct taxes paid to date
@@ -166,6 +163,94 @@ namespace TaxCalculator
 
             }
 
+            // Prompt and store the person's income tax deductions for the year in the totalTaxDeductions variable 
+            // after the user input is converted to a double and 
+            // it is validated agianst the taxes paid to date minimum and maximum range
+            // and we also know it is a number.
+            Console.Write("Please enter the person's total tax deductions for the year \n\t(between {0:c} and {1:c}): ", taxesDeductionsMinimum, taxesDeductionsMaximum); // prompt for the income tax deductions for the year
+
+            // Keep asking the user until they input the correct taxes paid to date
+            while ((!(Double.TryParse(Console.ReadLine(), out totalTaxDeductions)))        // check that the input is a double
+                    || ((totalTaxDeductions < taxesDeductionsMinimum)                      // compare the input against the minimum constant
+                    || (totalTaxDeductions > taxesDeductionsMaximum)))                     // compare the input against the maximum constant
+            {
+                // Prompt the user for the person's taxes paid to date.
+                Console.Write("\n Error \nPlease enter the person's total tax deductions for the year \n\t(between {0:c} and {1:c}): ", taxesDeductionsMinimum, taxesDeductionsMaximum); // prompt for the income tax deductions for the year
+
+            }
+
+            ///////////////
+            // Processing
+            ///////////////
+
+            // Calculate the total taxable income, which is calculated by subtracting the 
+            // total yearly deductions from the total yearly income.
+            totalTaxableIncome = totalYearlyIncome - totalTaxDeductions;
+
+            // Determine the tax rate based on the taxable income
+            if(totalTaxableIncome < levelFirstIncome)           // If the totalTaxableIncome is less than levelFirstIncome
+            {
+                applicableTaxRate = rateFirstTax;               // Assign the rateFirstTax value to the applicableTaxRate
+            }
+            else if (totalTaxableIncome < levelSecondIncome)    // Otherwise, If the totalTaxableIncome is less than levelSecondIncome
+            {
+                applicableTaxRate = rateSecondTax;              // Assign the rateSecondTax value to the applicableTaxRate
+            }
+            else if (totalTaxableIncome < levelThirdIncome)     // Otherwise, If the totalTaxableIncome is less than levelThirdIncome
+            {
+                applicableTaxRate = rateThirdTax;               // Assign the rateThirdTax value to the applicableTaxRate
+            }
+            else if (totalTaxableIncome < levelFourthIncome)    // Otherwise, If the totalTaxableIncome is less than levelFourthIncome
+            {
+                applicableTaxRate = rateFourthTax;              // Assign the rateFourthTax value to the applicableTaxRate
+            }
+            else if (totalTaxableIncome < levelFifthIncome)     // Otherwise, If the totalTaxableIncome is less than levelFifthIncome
+            {
+                applicableTaxRate = rateFifthTax;               // Assign the rateFifthTax value to the applicableTaxRate
+            }
+            else                                                // Otherwise
+            {
+                applicableTaxRate = rateSixthTax;               // Assign the rateSixthTax value to the applicableTaxRate
+            }
+            
+            // Calculate the total tax payable by multiplying the total 
+            // taxable income by the applicable tax rate.
+            totalTaxPayable = totalTaxableIncome * applicableTaxRate;
+
+            // Calculate the total taxes currently due by subtracting the 
+            // taxes paid to date from the total tax payable.
+            totalTaxCurrentlyDue = totalTaxPayable - taxesPaidToDate;
+
+            // Determine the message fragment that will be displayed
+            if (totalTaxCurrentlyDue > 0)       // If the amount currenty owed is greater than zero
+            {
+                message = messageOwe;           // Assign the messageOwe value to the message
+            }
+            else if(totalTaxCurrentlyDue < 0)   // Otherwise, If the amount currenty owed is less than zero
+            {
+                message = messageAreOwed;       // Assign the messageAreOwed value to the message
+            }
+            else                                // Otherwise,
+            {
+                message = messageOweNothing;    // Assign the messageOweNothing value to the message
+            }
+
+            ///////////////
+            // Output
+            ///////////////
+
+            // Show the user the results of the calculation.
+            Console.Write("\nWith a total yearly income of {0:c} and deductions of {1:c},\n" +
+                    "your taxable income is {2:c}.\nThis means your applicable tax rate is {3:p}.\n" +
+                    "Since you already paid {4:c} and your total tax payable is {5:c};\nyou... {6}, {7:c}.", 
+                        totalYearlyIncome, 
+                        totalTaxDeductions, 
+                        totalTaxableIncome, 
+                        applicableTaxRate, 
+                        taxesPaidToDate, 
+                        totalTaxPayable,
+                        message,
+                        totalTaxCurrentlyDue);
 
             // Ask the user to press any key to end the program
             // \n - use the newlijne escape character to move the output to the next line.
